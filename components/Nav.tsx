3a99290cf295f03ca301753255fcfe08ae673355
Santajit Patra <business.santajitpatra@gmail.com>
2023-10-18 02:23:11 +0530

    feat : shadcn/ui
    shadcn ui  setup

---
+++ components/Nav.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+import React from 'react'
+import { ModeToggle } from './ui/ModeToggle'
+
+const Nav = () => {
+    return (
+        <div>
+            <nav>
+                <ul className='flex items-center justify-between'>
+                    <li>
+                        <a href="https://santajitpatra.netlify.app"
+                            className='pointer-events-none flex place-items-center gap-2 p-8'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                        >
+                            <Image
+                            className='dark:invert'
+                                src={"/vercel.svg"}
+                                alt='vercel logo'
+                                width={100}
+                                height={24}
+                            />
+                        </a>
+                    </li>
+                    <li>
+                        <ModeToggle />
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav
